d936525c896b87ec87c141b231903db64d7fac5e
jucileiacabral <jucileiaa@hotmail.com>
2021-05-16 11:18:53 -0300

    primeiro commit

---
+++ DemoApi/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApi.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public Cliente(string texto)
+        {
+            this.Nome = texto;
+        }
+    }
+}
